aba83eb7a084d19d8738bae9a18c24f4417d9a14
sinarti <sinarti1999ir@gmail.com>
2019-05-14 04:50:12 +0430

    fix code

--- radiojavan-cli.py
+++ radiojavan-cli.py
@@ -30,8 +30,11 @@ elem = driver.find_element_by_xpath("""//*[@id="navContainer"]/div/a[1]""")
 actions = ActionChains(driver)
 actions.click(elem).perform()
 elem2 = driver.find_element_by_xpath("""//*[@id="playlist"]/div/div[1]/div/div[2]/a[2]""").click()
+po1 = driver.find_element_by_class_name("song").text
 while True:
-    next = input("\n\nNext music type(n) exit Type(e) Stop Type(s) Repeat Type(r)  max_window Type(maz)  min_window  Type(min):  ")
+    next = input("\n\n1-Next music type(n) \n\n2-exit Type(e) \n\n3-Stop Type(s) \n\n4-Repeat Type(r)  \n\n5-max_window Type(maz)  \n\n6-min_window  Type(min)   \n\n7-Downlaod Type(d) \n\n\nPlease Type:  ")
+    po1 = driver.find_element_by_class_name("song").text
+    po1 = driver.find_element_by_class_name("song").text
     if next == "n":
         driver.find_element_by_xpath("""//*[@id="mp3_next"]""").click()
     if next == "e":
@@ -47,8 +50,10 @@ while True:
     if next == "plus":
         driver.find_element_by_xpath("""//*[@id="mp3"]/div/div[3]/div[1]/div/div[3]/div[2]/div[2]/i[1]""")
     if next == "d":
+        po = driver.find_element_by_class_name("artist").text
+        print(po)
         link_rj="https://host2.rj-mw1.com/media/mp3/mp3-256/"
-        ar = input("========>> Please Enter artist name: ")
+        ar = po
         ar = ar.replace(',', '')
         ar = ar.replace('&', '')
         ar = ar.replace('vs', '')
@@ -58,7 +63,7 @@ while True:
         ar = ar.replace('(', '').replace(')', '')
         ar = ar.replace('[', '').replace(']', '')
         mp = "-".join(ar.split())
-        song =input("========>> Please Enter song name: ")
+        song = po1
         song = song.replace(',', '')
         song = song.replace('(', '').replace(')', '')
         song = song.replace('[', '').replace(']', '')
@@ -72,7 +77,7 @@ while True:
         newstr = "-".join((mp, md))
         ok=link_rj+newstr+mp3
         print(ok)
-        os.system("mkdir radiojavan_music_cli")
+        os.system("mkdir radiojavan_music")
         os.system("wget "+ok)
-        os.system("mv *.mp3 radiojavan_music_cli")
+        os.system("mv *.mp3 radiojavan_music")
 driver.close()
